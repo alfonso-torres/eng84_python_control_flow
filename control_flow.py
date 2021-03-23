199efd8b351525b7535e2ce34c35ca4d5ff817b1
alfonso-torres <alfonso_sepul@hotmail.com>
2021-03-23 15:26:18 +0000

    if, elif and else completed and exercise given.

---
+++ control_flow.py
@@ -0,0 +1,27 @@
+# Control Flow
+# if, else if/elif, else
+# Conditional statements are used to control the flow of our program
+
+age = 14
+# if the user is above the age of 15 allow them to buy a ticket
+if age >= 15: # If this condition is met/true the print statement will execute
+    print("Thank you, please proceed to your purchase.")
+
+# if the user is under 15 years of age do not allow to buy a ticket
+elif age < 15:
+    print("Sorry you are under age to watch this movie.")
+
+# else block gets executed if non of the above conditions met
+else:
+    print("Oops something went wrong, please try later.")
+
+# Exercise -
+# As a user I would like to sell tickets according to the age of the user
+# and category of the movie
+# age = 12a, PG, U, 15 and 18
+# user input to let us know the age to decide whether to sell the ticket or not
+# casting implemented
+# display the age back to the user with appropiate message
+
+
+# Ensure your git hub README documentation for todays lessons are up to date
