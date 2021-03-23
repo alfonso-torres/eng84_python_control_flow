728fbfe969358c626c1a46079d539d174a848bcf
alfonso-torres <alfonso_sepul@hotmail.com>
2021-03-23 16:56:49 +0000

    Exercise resolved and README.md updated.

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # Control flow
+A program's control flow is the order in which the program's code executes. The control flow of a Python program is regulated by conditionals statements, loops, and functions calls. Let's see how each of them works.
 
-## if, else if - elif and else
+### Conditional statements: *if, else if - elif and else*
+Conditional Statement in Python perform different computations or actions depending on whether a specific Boolean constraint evaluates to true or false. <br/>
+Syntax:<br/>
+````python
+if expression
+ Statement
+elif expression 
+ Statement
+else
+ Statement
+````
+Let's see an example:
+- We check if the number is positive.<br/>
+````python
+if num > 0:
+    print("Positive number")
+````
+- We check if the number is equal to 0. <br/>
+````python
+elif num == 0:
+    print("Zero")
+````
+- Otherwise, the number is negative. <br/>
+````python
+else:
+    print("Negative number")
+````
+When variable `num` is positive, `Positive number` is printed. <br/>
+If `num` is equal to 0, `Zero` is printed. <br/>
+If `num` is negative, `Negative number` is printed.
 ## For loops
 ## while loops
--- control_flow.py
+++ control_flow.py
@@ -15,6 +15,8 @@ elif age < 15:
 else:
     print("Oops something went wrong, please try later.")
 
+print("--------------------------------------------------------------------------------")
+
 # Exercise -
 # As a user I would like to sell tickets according to the age of the user
 # and category of the movie
@@ -23,5 +25,25 @@ else:
 # casting implemented
 # display the age back to the user with appropiate message
 
+name = input("Please insert your name: ")
+age = int(input("Please insert your age: "))
 
+if age < 12:
+    print("Hi " + name + ". Your age is " + str(age) + ", so you are in the category 12A. Those under 12 admitted, but only if accompanied by an adult(cinema only).")
+    print("Advice is given regarding the content of the film and the adult must decide if it is appropriate for the accompanying under-12.")
+    print("You are allowed to watch films with a 'U' certificate for everyone and 'PG' for anyone over the age of 8 years.")
+elif age >= 12 and age < 15:
+    print("Hi " + name + ". Your age is " + str(age) + ", so you are in the category 12. For the reason that you are in the range of 12 and 15 aged, you can watch the film unaccompanied.")
+    print("A 12 rating is given to any film deemed to be suitable for those older than 12 years.")
+    print("You are allowed to watch films with a 'U' and 'PG' certificates, but not with '15', '18' and 'R18' certificates.")
+elif age >= 15 and age < 18:
+    print("Hi " + name + ". Your age is " + str(age) + ", so you are in the category 15.")
+    print("A 15 rated films are aimed at a 'more nature' audience, meaning specifically teenagers, and grown ups.")
+    print("You are allowed to watch films with a 'U','PG' and '15' certificates, but not with '18' and 'R18' certificates.")
+else:
+    print("Hi " + name + ". Your age is " + str(age) + ", so you are in the category 18.")
+    print("A 18 rated film is designed specifically for adults.")
+    print("You are allowed to watch any film with any type of certificate, as long as you are 18 or older.")
+
+print("--------------------------------------------------------------------------------")
 # Ensure your git hub README documentation for todays lessons are up to date
